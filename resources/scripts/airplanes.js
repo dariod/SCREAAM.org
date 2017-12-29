e6029951332776d5ce3b698b4e33fe6bdb1d5f60
Dario Dorella <dario.dorella@gmail.com>
2017-12-29 11:39:38 +0100

    Make airplane dynamically fit client window.

--- resources/scripts/airplanes.js
+++ resources/scripts/airplanes.js
@@ -1,6 +1,26 @@
 var airplane
 var flightInterval
 
+$(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+    this.resizeTO = setTimeout(function() {
+        $(this).trigger('resizeEnd');
+    }, 500);
+});
+
+
+$(window).bind('resizeEnd', function() {
+    if ( window.innerWidth > window.innerHeight ) {
+        // If window is wider than taller, fit airplane height
+        $("#airplane svg").height("94vh")
+        $("#airplane").css("top","3vh")
+    } else {
+        // If window is taller than wider, fir airplane width
+        $("#airplane svg").width("94vw")
+        $("#airplane").css("top",(window.innerHeight - $("#airplane svg").height) / 2 + "px")
+    }
+});
+
 // function flyBy(airplane, jetnoise) {
 function flyBy(airplane) {
 
@@ -14,10 +34,6 @@ function flyBy(airplane) {
     airplane.offset({
             left: document.body.clientWidth,
         })
-
-    // Set the airplane SVG dimensions to fit the view
-    $("#airplane").css("top","3vh")
-    $("#airplane svg").height("94vh")
 
     // Make the plane visible (should be hidden on page load)
     airplane.css("visibility","visible")
@@ -41,14 +57,18 @@ document.onreadystatechange = function() {
 
         airplane=$("#airplane")
 
+        // Set the airplane SVG dimensions to some reasonable size
+        $("#airplane").css("top","3vh")
+        $("#airplane svg").height("94vh")
+
         // Set up randomly timed fly bys
         // Every so much time, we roll a dice and see if a jet airplane
         // will bother your website reading.
         var flightInterval = window.setInterval(
                 function() {
-                    if (Math.random() > 0.5) {
+                    // if (Math.random() > 0.5) {
                         flyBy(airplane)
-                }
+                // }
             },
             3000
         );
