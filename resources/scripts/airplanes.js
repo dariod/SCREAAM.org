f9d029fa39409bc38a922a819731a2b551ba57bd
Dario Dorella <dario.dorella@gmail.com>
2017-12-29 08:39:18 +0100

    Removed audio until I can figure it out.

--- resources/scripts/airplanes.js
+++ resources/scripts/airplanes.js
@@ -1,44 +1,53 @@
-var jetNoise
 var airplane
+var flightInterval
 
-function flyBy(airplane, jetnoise) {
+// function flyBy(airplane, jetnoise) {
+function flyBy(airplane) {
+
+    if (airplane.is(':animated')) {
+        // There is already an airplane passing by.
+        // No need to trigger another one.
+        return
+    }
 
     // Set up airplane initial position
     airplane.offset({
-            left: document.body.clientWidth
+            left: document.body.clientWidth,
+            top: 50
         })
 
     // Make the plane visible (should be hidden on page load)
     airplane.css("visibility","visible")
 
     // Animate!
-
-        airplane.animate({
-            left: (-1 * airplane.width() )
-        },{
-            duration: jetnoise.duration * 1000,
-            easing: "linear",
-            start: function() {
-                    jetnoise.play()
-                },
-            stop: function() {
-                    jetnoise.currentTime = 0;
-                }
-        })
+    airplane.animate({
+        left: (-1 * airplane.width() )
+    },{
+        // duration: jetnoise.duration * 1000,
+        duration: 15000,
+        easing: "linear"
+    })
 
 }
 
+
 // alternative to DOMContentLoaded event
 document.onreadystatechange = function() {
-    if (document.readyState === "interactive") {
-        console.log("Document reached interactive state.")
-
-        // Setup audio
-        console.log("Setting up audio.")
-        jetNoise = document.createElement('audio');
-        jetNoise.setAttribute('src', 'resources/audio/jetNoise.mp3');
-        jetNoise.setAttribute('id', 'jetnoise');
+    if (document.readyState === "complete") {
+        console.log("Document reached complete state.")
 
         airplane=$("#airplane")
+
+        // Set up randomly timed fly bys
+        // Every so much time, we roll a dice and see if a jet airplane
+        // will bother your website reading.
+        var flightInterval = window.setInterval(
+                function() {
+                    if (Math.random() > 0.5) {
+                        flyBy(airplane)
+                }
+            },
+            3000
+        );
     }
 };
