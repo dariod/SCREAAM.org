92e022cb6bd3208db7c46a0819cdafc48fa57346
Dario Dorella <dario.dorella@gmail.com>
2017-12-30 10:12:00 +0100

    Add safari sound.

--- resources/scripts/airplanes.js
+++ resources/scripts/airplanes.js
@@ -39,22 +39,93 @@ function flyBy(airplane) {
             left: document.body.clientWidth,
         })
 
+    // bufferSound
+    bufferSound()
+
     // Animate!
     airplane.animate({
         left: (-1 * airplane.width() )
     },{
         // duration: jetnoise.duration * 1000,
-        duration: 15000,
+        duration: webkitJetNoise.source.buffer.duration * 1000,
         easing: "linear",
         start: function () {
+                console.log("Animation start.")
                 // Make the plane visible (should be hidden on page load)
                 airplane.css("visibility","visible")
+                playSound(webkitJetNoise.source)
             },
         done: function () {
+                console.log("Animation stop.")
                 // Make the plane visible (should be hidden on page load)
                 airplane.css("visibility","hidden")
             }
     })
+
+}
+
+// This is part of Safari sound handling
+
+var webkitJetNoise = {
+    audioContext: null,
+    buffer: null,
+    source: null
+}
+
+
+// progress on transfers from the server to the client (downloads)
+function updateProgress (oEvent) {
+  if (oEvent.lengthComputable) {
+    var percentComplete = oEvent.loaded / oEvent.total;
+    // ...
+  } else {
+    // Unable to compute progress information since the total size is unknown
+  }
+}
+
+function transferComplete(evt) {
+  console.log("The transfer is complete.");
+}
+
+function transferFailed(evt) {
+  console.log("An error occurred while transferring the file.");
+}
+
+function transferCanceled(evt) {
+  console.log("The transfer has been canceled by the user.");
+}
+
+function stashDownloadedSound(event) {
+    webkitJetNoise.buffer = event.target.response
+    console.log("The jet noise sound transfer is complete.")
+}
+
+function bufferSound() {
+    webkitJetNoise.source = webkitJetNoise.audioContext.createBufferSource()
+    webkitJetNoise.source.buffer = webkitJetNoise.audioContext.createBuffer(webkitJetNoise.buffer, false)
+    webkitJetNoise.source.connect(webkitJetNoise.audioContext.destination)
+}
+
+function playSound(source) {
+    source.start()
+}
+
+// Init sound for Safari
+if('webkitAudioContext' in window) {
+
+    webkitJetNoise.audioContext = new webkitAudioContext();
+
+
+    // Buffer the sound`
+    request = new XMLHttpRequest();
+    request.open('GET', '/resources/audio/jetNoise.mp3', true);
+    request.responseType = 'arraybuffer';
+    request.addEventListener('load', stashDownloadedSound, false);
+    // request.addEventListener("load", transferComplete);
+    request.addEventListener("progress", updateProgress);
+    request.addEventListener("error", transferFailed);
+    request.addEventListener("abort", transferCanceled);
+    request.send();
 
 }
 
@@ -70,12 +141,22 @@ document.onreadystatechange = function() {
         // Will refit at every window resize.
         fitPlane(airplane)
 
+        // The below does not work on Safari:
+        //
+        // jetNoise = document.createElement('audio');
+        // jetNoise.setAttribute('src', 'resources/audio/jetNoise.mp3');
+        // jetNoise.setAttribute('preload', 'auto');
+        // jetNoise.setAttribute('type', 'audio/mpeg')
+        // jetNoise.setAttribute('id', 'jetnoise');
+
+
+
         // Set up randomly timed fly bys
         // Every so much time, we roll a dice and see if a jet airplane
         // will bother your website reading.
         var flightInterval = window.setInterval(
                 function() {
-                    if (Math.random() > 0.8) {
+                    if (Math.random() > 0.4) {
                         flyBy(airplane)
                 }
             },
