68ebfade1af71e11896eaec5cf6055a077d43e24
Dario Dorella <dario.dorella@gmail.com>
2017-12-30 10:43:31 +0100

    Github raw=true fix

--- resources/scripts/airplanes.js
+++ resources/scripts/airplanes.js
@@ -118,7 +118,7 @@ if('webkitAudioContext' in window) {
 
     // Buffer the sound`
     request = new XMLHttpRequest();
-    request.open('GET', '/resources/audio/jetNoise.mp3', true);
+    request.open('GET', 'https://github.com/dariod/SCREAAM.org/blob/gh-pages/resources/audio/jetNoise.mp3?raw=true', true);
     request.responseType = 'arraybuffer';
     request.addEventListener('load', stashDownloadedSound, false);
     // request.addEventListener("load", transferComplete);
